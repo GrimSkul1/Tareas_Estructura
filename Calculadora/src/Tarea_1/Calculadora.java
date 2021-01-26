9ec711ae2a41f30d975e2bf2a04f20fc37b98b5b
bryan <bryan@bryanlegion>
2021-01-26 01:22:36 -0400

    Agg binarios

--- Calculadora/src/Tarea_1/Calculadora.java
+++ Calculadora/src/Tarea_1/Calculadora.java
@@ -15,12 +15,14 @@ import java.awt.event.MouseEvent;
 import javax.swing.JTextPane;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class Calculadora extends JFrame {
 
 	private JPanel contentPane;
 	Calculos calc = new Calculos();
 	private JTextField textField;
+	private JTextField textBinario;
 
 	/**
 	 * Launch the application.
@@ -44,7 +46,7 @@ public class Calculadora extends JFrame {
 	 */
 	public Calculadora() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 402, 499);
+		setBounds(100, 100, 330, 533);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -52,7 +54,7 @@ public class Calculadora extends JFrame {
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(153, 153, 153));
-		panel.setBounds(0, 0, 384, 456);
+		panel.setBounds(0, 0, 312, 486);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -65,7 +67,7 @@ public class Calculadora extends JFrame {
 		});
 		btn1.setBackground(new Color(255, 204, 204));
 		btn1.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btn1.setBounds(26, 118, 66, 72);
+		btn1.setBounds(10, 133, 66, 72);
 		panel.add(btn1);
 		
 		JButton btn2 = new JButton("2");
@@ -77,7 +79,7 @@ public class Calculadora extends JFrame {
 		});
 		btn2.setBackground(new Color(255, 204, 204));
 		btn2.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btn2.setBounds(101, 118, 66, 72);
+		btn2.setBounds(85, 133, 66, 72);
 		panel.add(btn2);
 		
 		JButton btn3 = new JButton("3");
@@ -89,7 +91,7 @@ public class Calculadora extends JFrame {
 		});
 		btn3.setBackground(new Color(255, 204, 204));
 		btn3.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btn3.setBounds(177, 118, 66, 72);
+		btn3.setBounds(161, 133, 66, 72);
 		panel.add(btn3);
 		
 		JButton btn4 = new JButton("4");
@@ -101,7 +103,7 @@ public class Calculadora extends JFrame {
 		});
 		btn4.setBackground(new Color(255, 204, 204));
 		btn4.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btn4.setBounds(26, 201, 66, 72);
+		btn4.setBounds(10, 216, 66, 72);
 		panel.add(btn4);
 		
 		JButton btn5 = new JButton("5");
@@ -114,7 +116,7 @@ public class Calculadora extends JFrame {
 		});
 		btn5.setBackground(new Color(255, 204, 204));
 		btn5.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btn5.setBounds(101, 201, 66, 72);
+		btn5.setBounds(85, 216, 66, 72);
 		panel.add(btn5);
 		
 		JButton btn6 = new JButton("6");
@@ -126,7 +128,7 @@ public class Calculadora extends JFrame {
 		});
 		btn6.setBackground(new Color(255, 204, 204));
 		btn6.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btn6.setBounds(177, 200, 66, 73);
+		btn6.setBounds(161, 215, 66, 73);
 		panel.add(btn6);
 		
 		JButton btn7 = new JButton("7");
@@ -138,7 +140,7 @@ public class Calculadora extends JFrame {
 		});
 		btn7.setBackground(new Color(255, 204, 204));
 		btn7.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btn7.setBounds(26, 280, 66, 72);
+		btn7.setBounds(10, 295, 66, 72);
 		panel.add(btn7);
 		
 		JButton btn8 = new JButton("8");
@@ -150,7 +152,7 @@ public class Calculadora extends JFrame {
 		});
 		btn8.setBackground(new Color(255, 204, 204));
 		btn8.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btn8.setBounds(101, 280, 66, 72);
+		btn8.setBounds(85, 295, 66, 72);
 		panel.add(btn8);
 		
 		JButton btn9 = new JButton("9");
@@ -162,19 +164,18 @@ public class Calculadora extends JFrame {
 		});
 		btn9.setBackground(new Color(255, 204, 204));
 		btn9.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btn9.setBounds(177, 280, 66, 72);
+		btn9.setBounds(161, 295, 66, 72);
 		panel.add(btn9);
 		
 		JButton btnSuma = new JButton("+");
-		btnSuma.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
+		btnSuma.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				calc.suma(textField.getText());
 			}
 		});
 		btnSuma.setBackground(new Color(255, 255, 255));
 		btnSuma.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btnSuma.setBounds(253, 118, 106, 72);
+		btnSuma.setBounds(237, 133, 66, 72);
 		panel.add(btnSuma);
 		
 		JButton btnResta = new JButton("-");
@@ -184,38 +185,33 @@ public class Calculadora extends JFrame {
 			}
 			
 		});
-		btnResta.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-			}
-		});
 		btnResta.setBackground(new Color(255, 255, 255));
 		btnResta.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btnResta.setBounds(253, 201, 106, 72);
+		btnResta.setBounds(237, 216, 66, 72);
 		panel.add(btnResta);
 		
 		JButton btnMultpl = new JButton("x");
-		btnMultpl.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
+		btnMultpl.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				calc.multpl(textField.getText());
 			}
 		});
+		
 		btnMultpl.setBackground(new Color(255, 255, 255));
 		btnMultpl.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btnMultpl.setBounds(253, 280, 106, 72);
+		btnMultpl.setBounds(237, 295, 66, 72);
 		panel.add(btnMultpl);
 		
 		JButton btnDivision = new JButton("/");
-		btnDivision.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
+		btnDivision.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
 				calc.division(textField.getText());
 			}
 		});
+		
 		btnDivision.setBackground(new Color(255, 255, 255));
 		btnDivision.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btnDivision.setBounds(253, 363, 106, 72);
+		btnDivision.setBounds(237, 378, 66, 72);
 		panel.add(btnDivision);
 		
 		JButton btn0 = new JButton("0");
@@ -227,24 +223,25 @@ public class Calculadora extends JFrame {
 		});
 		btn0.setBackground(new Color(255, 204, 204));
 		btn0.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btn0.setBounds(26, 363, 66, 72);
+		btn0.setBounds(10, 378, 66, 72);
 		panel.add(btn0);
 		
 		JButton btnResultado = new JButton("=");
-		btnResultado.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
+		btnResultado.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
 				textField.setText(""+calc.resultado(textField.getText()));
+				textBinario.setText(""+calc.convertirBinario(textField.getText()));
 			}
 		});
+		
 		btnResultado.setBackground(new Color(255, 255, 255));
 		btnResultado.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btnResultado.setBounds(177, 363, 66, 72);
+		btnResultado.setBounds(161, 378, 66, 72);
 		panel.add(btnResultado);
 		
 		textField = new JTextField();
 		textField.setFont(new Font("Tahoma", Font.PLAIN, 50));
-		textField.setBounds(26, 35, 333, 72);
+		textField.setBounds(10, 62, 293, 60);
 		panel.add(textField);
 		textField.setColumns(10);
 		
@@ -253,10 +250,21 @@ public class Calculadora extends JFrame {
 		btnClear.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textField.setText(calc.clear(""));
+				textBinario.setText(calc.clear(""));
 			}
 		});
 		btnClear.setFont(new Font("Tahoma", Font.BOLD, 22));
-		btnClear.setBounds(101, 363, 66, 72);
+		btnClear.setBounds(85, 378, 66, 72);
 		panel.add(btnClear);
+		
+		JLabel lblNewLabel = new JLabel("Resultado en Binario");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel.setBounds(10, 26, 142, 25);
+		panel.add(lblNewLabel);
+		
+		textBinario = new JTextField();
+		textBinario.setBounds(161, 24, 142, 27);
+		panel.add(textBinario);
+		textBinario.setColumns(10);
 	}
 }
--- Calculadora/src/Tarea_1/Calculos.java
+++ Calculadora/src/Tarea_1/Calculos.java
@@ -3,7 +3,7 @@ package Tarea_1;
 public class Calculos {
 
 	private String pantalla;
-	private float resultado;
+	private int resultado;
 	private boolean suma;
 	private boolean resta;
 	private boolean multpl;
@@ -23,44 +23,52 @@ public class Calculos {
 	}
 	
 	public void suma (String pantalla) {
-		this.resultado = Float.parseFloat(pantalla);
+		this.resultado = Integer.parseInt(pantalla);
 		suma = true;
 		this.pantalla = "";
 	}
 	
 	public void resta (String pantalla) {
-		this.resultado = Float.parseFloat(pantalla);
+		this.resultado = Integer.parseInt(pantalla);
 		resta = true;
 		this.pantalla = "";
 	}
 	
 	public void multpl (String pantalla) {
-		this.resultado = Float.parseFloat(pantalla);
+		this.resultado = Integer.parseInt(pantalla);
 		multpl = true;
 		this.pantalla = "";
 	}
 	
 	public void division (String pantalla) {
-		this.resultado = Float.parseFloat(pantalla);
+		this.resultado = Integer.parseInt(pantalla);
 		division =  true;
 		this.pantalla = "";
 	}
 	
 	public String clear (String pantalla) {
 		this.pantalla = "";
+		suma = false;
+		resta = false;
+		multpl = false;
+		division = false;
 		return pantalla;
 	}
 	
-	public float resultado (String numero) {
+	
+	public int resultado (String numero) {
 		
 		if (suma = true) {
-			resultado = resultado + Float.parseFloat(numero);
-		}else if (resta = true) {
-			resultado = resultado + Float.parseFloat(numero);
-		}else if (multpl = true) {
-			resultado = resultado + Float.parseFloat(numero);
-		}else if (division = true) {
-			resultado = resultado + Float.parseFloat(numero);
+			resultado = resultado + Integer.parseInt(numero);
+		}
+		else if (resta = true) {
+			resultado = resultado - Integer.parseInt(numero);
+		}
+		else if (multpl = true) {
+			resultado = resultado / Integer.parseInt(numero);
+		}
+		else if (division = true) {
+			resultado = resultado * Integer.parseInt(numero);
 		}
 		
 		suma = false;
@@ -69,8 +77,26 @@ public class Calculos {
 		division = false;
 		
 		return resultado;
-		
-		
 	}
+	
+	public String convertirBinario (String resultado) {
+		
+		int count = 0, a;
+	    int n = Integer.parseInt(resultado);
+		String x = "";
+		while(n > 0)
+        {
+            a = n % 2;
+            if(a == 1)
+            {
+                count++;
+            }
+            x = a + "" + x;
+            n = n / 2;
+        }
+		
+		return x;
+	}
+	
 	
 }
